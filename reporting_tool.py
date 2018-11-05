4626d5d2cf0f7f7c61083d56cc1ecebb4f3c652e
Chris Vasquez <chrvasq@outlook.com>
2018-11-04 20:53:19 -0800

    Add formatting

--- reporting_tool.py
+++ reporting_tool.py
@@ -4,5 +4,16 @@ import psycopg2
 
 DBNAME = 'news'
 
-db = psycopg2.connect(database=DBNAME)
 
+def top_three():
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+
+    c.execute("select path, count(*) from log where status = '200 OK' and path != '/' group by log.path;")
+    results = c.fetchall()
+    db.close()
+
+    for article in results:
+        print('{} -  {} views'.format(article[0], article[1]))
+
+top_three()
