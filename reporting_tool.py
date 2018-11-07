45944b38487c50642d95e1d6c559e44567c6faa4
Chris Vasquez <chrvasq@outlook.com>
2018-11-06 19:52:23 -0800

    Add solution #2

--- reporting_tool.py
+++ reporting_tool.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
 import psycopg2
 
@@ -6,6 +6,9 @@ DBNAME = 'news'
 
 
 def top_three():
+    print('\n')
+    print('Processing first report...\n')
+
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
 
@@ -13,10 +16,32 @@ def top_three():
     results = c.fetchall()
     db.close()
 
-    print('Top three articles of all time: ')
-    
+    print('Top three articles of all time: \n')
+
     for item in results:
         print('\"{}\" - {} views'.format(item[0], item[1]))
 
+    print('\n')
+
+
+def popular_article_authors():
+    print('Processing second report...\n')
+
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+
+    c.execute("select name, sum(article_views.num) as total_views from authors join (select articles.author as author, title, count(*) as num from articles join log on log.path like concat('%', articles.slug) group by articles.author, articles.title order by num desc) as article_views on authors.id = article_views.author group by authors.name order by total_views desc;")
+    results = c.fetchall()
+    db.close()
+
+    print('Article authors ranked by views: \n')
+
+    for item in results:
+        print('{} - {} views'.format(item[0], item[1]))
+
+    print('\n')
+
+
 if __name__ == '__main__':
     top_three()
+    popular_article_authors()
