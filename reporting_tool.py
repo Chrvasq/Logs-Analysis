bac232ece9c4fa02cc3d8f14d78e9bdb441ea0a7
Chris Vasquez <chrvasq@outlook.com>
2018-11-07 20:42:39 -0800

    Fix formatting and add example.txt file

--- reporting_tool.py
+++ reporting_tool.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import psycopg2
+from datetime import datetime
 
 DBNAME = 'news'
 
@@ -12,7 +13,11 @@ def top_three():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
 
-    c.execute("select title, count(*) as num from articles join log on log.path like concat('%', articles.slug) group by articles.title order by num desc limit 3")
+    c.execute(
+        "select title, count(*) as num from articles join log on"
+        " log.path like concat('%', articles.slug) group by"
+        " articles.title order by num desc limit 3"
+        )
     results = c.fetchall()
     db.close()
 
@@ -30,7 +35,14 @@ def popular_article_authors():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
 
-    c.execute("select name, sum(article_views.num) as total_views from authors join (select articles.author as author, title, count(*) as num from articles join log on log.path like concat('%', articles.slug) group by articles.author, articles.title order by num desc) as article_views on authors.id = article_views.author group by authors.name order by total_views desc;")
+    c.execute(
+        "select name, sum(article_views.num) as total_views from authors join"
+        "(select articles.author as author, title, count(*) as num from"
+        " articles join log on log.path like concat('%', articles.slug) group"
+        " by articles.author, articles.title) as article_views on authors.id ="
+        " article_views.author group by authors.name order by total_views"
+        " desc;"
+        )
     results = c.fetchall()
     db.close()
 
@@ -41,22 +53,35 @@ def popular_article_authors():
 
     print('\n')
 
+
 def error_report():
     print('Processing third report...\n')
 
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
 
-    c.execute("select t3.time::date, t3.error_percentage from (select t1.time::date, (t1.total / t2.total) * 100 as error_percentage from (select a.time::date, sum(a.num) as total from (select status, time::date, count(*) as num from log where status != '200 OK' group by log.status, log.time::date order by status, time) as a group by a.time::date order by time) as t1, (select b.time::date, sum(b.num) as total from (select status, time::date, count(*) as num from log group by log.status, log.time::date order by status, time) as b group by b.time::date order by time) as t2 where t1.time = t2.time) as t3 where error_percentage > 1;")
+    c.execute(
+        "select t3.time::date, t3.error_percentage from (select t1.time::date,"
+        " (t1.total / t2.total) * 100 as error_percentage from (select"
+        " a.time::date, sum(a.num) as total from (select status, time::date,"
+        " count(*) as num from log where status != '200 OK' group by"
+        " log.status, log.time::date) as a group by a.time::date) as t1"
+        ", (select b.time::date, sum(b.num) as total from (select status"
+        ", time::date, count(*) as num from log group by log.status"
+        ", log.time::date) as b group by b.time::date) as t2 where"
+        " t1.time = t2.time) as t3 where error_percentage > 1;"
+        )
     results = c.fetchall()
     db.close()
 
+    date = results[0][0]
+    percent = round(results[0][1], 2)
+
     print('Failed Requests > 1%: \n')
 
-    for item in results:
-        print('{} - {} errors'.format(item[0], item[1]))
+    print(date.strftime('%B %d, %Y - ') + str(percent) + '% ' + 'errors\n')
 
-    print('\n End of report.')
+    print('End of report.\n')
 
 
 if __name__ == '__main__':
